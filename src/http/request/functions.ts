40048e86cc414e4b2ea9e366ccd746d4599b7e14
Sean T. <webdeveloper@sympatico.ca>
2025-11-17 11:23:32 -0500

    Update functions.ts

--- src/http/request/functions.ts
+++ src/http/request/functions.ts
@@ -1,5 +1,11 @@
 import { RequestContext } from "./interfaces";
 
 export function getIpAddress(request: RequestContext): string {
-  return (request.headers["x-forwarded-for"] as string) || request.ip;
+  const forwardedForIps = request.headers["x-forwarded-for"] as string;
+
+  const firstForwardedForIp = forwardedForIps
+    ? forwardedForIps.split(",")[0].trim()
+    : null;
+
+  return firstForwardedForIp || request.ip;
 }
