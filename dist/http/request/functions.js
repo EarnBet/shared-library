2c2f5127536d5b78a0247b2359feeb0e929fdac5
Sean T. <webdeveloper@sympatico.ca>
2025-11-17 11:34:23 -0500

    build

--- dist/http/request/functions.js
+++ dist/http/request/functions.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getIpAddress = getIpAddress;
 function getIpAddress(request) {
-    return request.headers["x-forwarded-for"] || request.ip;
+    const forwardedForIps = request.headers["x-forwarded-for"];
+    const firstForwardedForIp = forwardedForIps
+        ? forwardedForIps.split(",")[0].trim()
+        : null;
+    return firstForwardedForIp || request.ip;
 }
 //# sourceMappingURL=functions.js.map
--- dist/users/repositories/functions.d.ts
+++ dist/users/repositories/functions.d.ts
@@ -2,6 +2,6 @@ export declare function whereClauseForSimilarUsername({ columnName, username, }:
     columnName: string;
     username: string;
 }): {
-    [x: string]: import("typeorm").FindOperator<any>;
+    [columnName]: import("typeorm").FindOperator<any>;
 };
 //# sourceMappingURL=functions.d.ts.map
