bb7f7b87a6c316fba881a7114205b826bdc8dd33
`mrt <martynas@jusys.lt>
2026-03-02 14:40:51 +0200

    build of added new admin roles.

--- dist/users/repositories/functions.d.ts
+++ dist/users/repositories/functions.d.ts
@@ -2,6 +2,6 @@ export declare function whereClauseForSimilarUsername({ columnName, username, }:
     columnName: string;
     username: string;
 }): {
-    [columnName]: import("typeorm").FindOperator<any>;
+    [x: string]: import("typeorm").FindOperator<any>;
 };
 //# sourceMappingURL=functions.d.ts.map
